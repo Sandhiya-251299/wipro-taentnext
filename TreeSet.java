637bdb44eed09253286c325138fb676107871377
SANDHIYA VELUMANI <64846988+Sandhiya-251299@users.noreply.github.com>
2020-06-11 19:06:36 +0530

    Create TreeSet.java

---
+++ TreeSet.java
@@ -0,0 +1,27 @@
+ class TreeSet{
+
+	public static void main(String[] args) {
+		TreeSet<String> set = new TreeSet<>();
+		
+		set.add("Bob");
+		set.add("Alice");
+		set.add("John");
+		set.add("Richard");
+		
+		Iterator<String> it = set.iterator();
+		String query = "John";
+		boolean result = false;
+		
+		while (it.hasNext()) {
+			if (it.next().equals(query)) {
+				result = true;
+				break;
+			}
+		}
+		
+		if (result) System.out.println(query + " exists");
+		else System.out.println(query + " doesn't exist");
+
+	}
+
+}
